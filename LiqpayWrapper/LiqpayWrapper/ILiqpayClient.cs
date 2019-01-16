bcabc928064c49469bf9941be47eb4ebed05a84b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 15:19:57 +0200

    Update ILiqpayClient.cs

--- LiqpayWrapper/LiqpayWrapper/ILiqpayClient.cs
+++ LiqpayWrapper/LiqpayWrapper/ILiqpayClient.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LiqpayWrapper.Data.Requests;
+using LiqpayWrapper.Data.Responses;
 
 namespace LiqpayWrapper
 {
     interface ILiqpayClient
     {
+        StatusResponse GetStatus(StatusRequest model);
+
+        OtpVerifyResponse OtpVerify(OtpVerifyRequest model);
+
+        Verify3DsResponse Verify3Ds(Verify3DsRequest model);
     }
 }
