294c389beaec105c8846a19e0fb2c433214e3940
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 13:40:03 +0200

    Create PayResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Responses/Acquiring/PayResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Responses.Acquiring
+{
+    public class PayResponse : AcquirerResponse
+    {
+        [JsonProperty("redirect_to")]
+        public string RedirectTo { get; private set; }
+
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("card_token")]
+        public string CardToken { get; private set; }
+    }
+}
