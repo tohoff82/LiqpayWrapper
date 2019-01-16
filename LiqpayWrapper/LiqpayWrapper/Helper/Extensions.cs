47633734235827959a9ba613679880019ffdf9af
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 15:55:41 +0200

    Update Extensions.cs

--- LiqpayWrapper/LiqpayWrapper/Helper/Extensions.cs
+++ LiqpayWrapper/LiqpayWrapper/Helper/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace LiqpayWrapper.Helper
@@ -6,5 +8,7 @@ namespace LiqpayWrapper.Helper
     public static class Extensions
     {
         public static async Task<string> ToJson(this object obj) => await Task.Run(() => JsonConvert.SerializeObject(obj));
+
+        public static string ToBase64(this Task<string> str) => Convert.ToBase64String(Encoding.UTF8.GetBytes(str.Result));
     }
 }
