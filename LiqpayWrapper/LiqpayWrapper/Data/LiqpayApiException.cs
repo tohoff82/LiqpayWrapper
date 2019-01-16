00bd1083d7d382f1527d5c6c24c331f3bda0a969
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 18:17:05 +0200

    Create LiqpayApiException.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/LiqpayApiException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace LiqpayWrapper.Data
+{
+    public class LiqpayApiException : HttpRequestException
+    {
+        public LiqpayApiException() : base() { }
+    }
+}
