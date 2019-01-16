4e67572342539767b10ae005d4b113cb5d45975b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 12:45:54 +0200

    Create BaseResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data
+{
+    public abstract class BaseResponse
+    {
+        [JsonProperty("err_code")]
+        public string ErrCode { get; private set; }
+
+        [JsonProperty("err_description")]
+        public string ErrDescription { get; private set; }
+
+        [JsonProperty("version")]
+        public int Version { get; private set; }
+    }
+}
