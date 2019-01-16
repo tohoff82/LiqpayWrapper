22ac3e124f039fb7b63128f65efe936c2e763a28
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 15:12:34 +0200

    Create Verify3DsRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/Verify3DsRequest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests
+{
+    public class VerifyRequest : BaseRequest
+    {
+        [JsonProperty("token", NullValueHandling = NullValueHandling.Ignore)]
+        public string Token { get; set; }
+    }
+}
