fe3a720cb620378d5d247f82ef1f6b56b1a10e11
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 14:56:52 +0200

    Create HoldCompletionRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/Acquiring/HoldCompletionRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests.Acquiring
+{
+    public class HoldCompletionRequest : BaseRequest
+    {
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+
+        [JsonProperty("order_id")]
+        public string OrderId { get; set; }
+    }
+}
