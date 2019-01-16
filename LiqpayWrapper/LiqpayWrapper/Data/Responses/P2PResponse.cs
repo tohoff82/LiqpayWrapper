dc6a9f854951677d8836e0a67c52b52907e57002
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 13:26:18 +0200

    Create P2PResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Responses/P2PResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Responses
+{
+    public class P2PResponse : BasePaymentResponse
+    {
+        [JsonProperty("redirect_to")]
+        public string RedirectTo { get; private set; }
+
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("card_token")]
+        public string CardToken { get; private set; }
+    }
+}
