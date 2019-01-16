68f3bcac55e02e499c6815615904a4c48b6d4763
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 13:16:33 +0200

    Create CallbackResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Responses/CallbackResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Responses
+{
+    public class CallbackResponse : BaseResponse
+    {
+        [JsonProperty("redirect_to")]
+        public string RedirectTo { get; private set; }
+
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("card_token")]
+        public string CardToken { get; private set; }
+
+        [JsonProperty("status")]
+        public string Status { get; private set; }
+    }
+}
