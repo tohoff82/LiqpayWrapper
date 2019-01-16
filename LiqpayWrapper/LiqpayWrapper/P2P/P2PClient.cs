0120496a804caea94e588303d9a5794deafffaf3
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 15:55:17 +0200

    Update P2PClient.cs

--- LiqpayWrapper/LiqpayWrapper/P2P/P2PClient.cs
+++ LiqpayWrapper/LiqpayWrapper/P2P/P2PClient.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using LiqpayWrapper.Data.Requests;
+using LiqpayWrapper.Data.Responses;
 
 namespace LiqpayWrapper.P2P
 {
     public class P2PClient : LiqpayClient, IP2PClient
     {
+        public P2PClient(string publicKey, string privateKey) : base(publicKey, privateKey) { }
+
+        public async Task<P2PResponse> P2P(P2PRequest model)
+        {
+            model.Action = "p2p";
+            return await Task.Run(() => Query<P2PResponse>(model));
+        }
+
+        public async Task<P2PCreditResponse> P2PCredit(P2PCreditRequest model)
+        {
+            model.Action = "p2pcredit";
+            return await Task.Run(() => Query<P2PCreditResponse>(model));
+        }
     }
 }
