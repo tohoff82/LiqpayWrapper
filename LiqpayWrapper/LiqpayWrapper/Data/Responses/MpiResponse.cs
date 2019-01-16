5c36c090e49036325a832dc4fc22832d9fa1d444
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 13:20:16 +0200

    Create MpiResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Responses/MpiResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Responses
+{
+    public class MpiResponse : BaseResponse
+    {
+        [JsonProperty("mpi_req_pareq")]
+        public string MpiReqPareq { get; private set; }
+
+        [JsonProperty("mpi_req_md")]
+        public string MpiReqMd { get; private set; }
+
+        [JsonProperty("mpi_req_url")]
+        public string MpiReqUrl { get; private set; }
+
+        [JsonProperty("mpi_status")]
+        public string MpiStatus { get; private set; }
+    }
+}
