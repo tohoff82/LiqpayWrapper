ce457bdfaae59b39ad99be5cf6e02b9f26a3b7a6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 12:37:33 +0200

    Create Extensions.cs
    ToJson Extension

---
+++ LiqpayWrapper/LiqpayWrapper/Helper/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Helper
+{
+    public static class Extensions
+    {
+        public static async Task<string> ToJson(this object obj) => await Task.Run(() => JsonConvert.SerializeObject(obj));
+    }
+}
