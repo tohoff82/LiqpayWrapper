d8fcd27f770d8ea7ea570ea9340519f980e6fa68
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 14:54:02 +0200

    Create StatusRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/StatusRequest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests
+{
+    public class StatusRequest : BaseRequest
+    {
+        [JsonProperty("order_id")]
+        public string OrderId { get; set; }
+    }
+}
