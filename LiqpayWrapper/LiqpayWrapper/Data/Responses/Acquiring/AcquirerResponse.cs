0f39c68af8fa981b07d1937450adfae2f7c358ed
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 13:39:59 +0200

    Create AcquirerResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Responses/Acquiring/AcquirerResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Responses.Acquiring
+{
+    public class AcquirerResponse : BasePaymentResponse
+    {
+        [JsonProperty("product_url")]
+        public string ProductUrl { get; private set; }
+
+        [JsonProperty("product_category")]
+        public string ProductCategory { get; private set; }
+
+        [JsonProperty("product_name")]
+        public string ProductName { get; private set; }
+
+        [JsonProperty("product_description")]
+        public string ProductDescription { get; private set; }
+    }
+}
