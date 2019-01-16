c1f09bd87f0f819e2c6084ffa29e2e1eb80ab206
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 14:54:06 +0200

    Create ReportRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/ReportRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests
+{
+    public class ReportRequest : BaseRequest
+    {
+        [JsonProperty("date_from")]
+        public string DateFrom { get; set; }
+
+        [JsonProperty("date_to")]
+        public string DateTo { get; set; }
+    }
+}
