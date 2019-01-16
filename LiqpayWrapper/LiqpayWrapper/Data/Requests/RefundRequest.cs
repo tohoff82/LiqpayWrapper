4e25ee9f9e30ad0311c7002aeefeb1047d22efde
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 14:45:07 +0200

    Create RefundRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/RefundRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests
+{
+    public class RefundRequest : BaseRequest
+    {
+        [JsonProperty("order_id")]
+        public string OrderId { get; set; }
+
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+    }
+}
