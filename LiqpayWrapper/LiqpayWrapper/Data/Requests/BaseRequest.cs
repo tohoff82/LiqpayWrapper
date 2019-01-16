1e82ac37595bb4d014860b81e9da7ee97463cfb3
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 14:33:04 +0200

    Create BaseRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/BaseRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests
+{
+    public abstract class BaseRequest
+    {
+        [JsonProperty("version")]
+        public int Version { get; set; }
+
+        [JsonProperty("public_key")]
+        public string PublicKey { get; set; }
+
+        [JsonProperty("action")]
+        public string Action { get; set; }
+
+        [JsonIgnore]
+        public string PrivateKey { get; set; }
+    }
+}
