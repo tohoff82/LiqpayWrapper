b593d4b8a6e1b5ea8539b53361274ee71a563062
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 15:19:44 +0200

    Update IAcquirerClient.cs

--- LiqpayWrapper/LiqpayWrapper/Acquiring/IAcquirerClient.cs
+++ LiqpayWrapper/LiqpayWrapper/Acquiring/IAcquirerClient.cs
@@ -1,10 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using LiqpayWrapper.Data.Requests;
+using LiqpayWrapper.Data.Requests.Acquiring;
+using LiqpayWrapper.Data.Responses.Acquiring;
+using LiqpayWrapper.Data.Responses;
 
 namespace LiqpayWrapper.Acquiring
 {
     interface IAcquirerClient : ILiqpayClient
     {
+        HoldResponse Hold(HoldRequest model);
+
+        HoldCompletionResponse HoldCompletion(HoldCompletionRequest model);
+
+        PayResponse Pay(PayRequest model);
+
+        SubscribeResponse Subscribe(SubscribeRequest model);
+
+        UnsubscribeResponse Unsubscribe(UnsubscribeRequest model);
+
+        MpiResponse Mpi(MpiRequest model);
+
+        AcquirerResponse Refund(RefundRequest model);
+
+        ReportResponse Reports(DateTime dateFrom, DateTime dateTo);
+
+        AuthResponse Auth(AuthRequest model);
     }
 }
