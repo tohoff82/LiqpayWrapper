32eac4dfe4f5992a13dc80fdfe358b64400296a0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 15:21:28 +0200

    Update IP2PClient.cs

--- LiqpayWrapper/LiqpayWrapper/P2P/IP2PClient.cs
+++ LiqpayWrapper/LiqpayWrapper/P2P/IP2PClient.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LiqpayWrapper.Data.Requests;
+using LiqpayWrapper.Data.Responses;
 
 namespace LiqpayWrapper.P2P
 {
     interface IP2PClient : ILiqpayClient
     {
+        P2PResponse P2P(P2PRequest model);
+
+        P2PCreditResponse P2PCredit(P2PCreditRequest model);
     }
 }
