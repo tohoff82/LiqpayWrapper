2c3d0becc119f63e4f166f846450919f2a083f79
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 14:43:45 +0200

    Create OtpVerifyRequest.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Requests/OtpVerifyRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Requests
+{
+   public class OtpVerifyRequest : BaseRequest
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("otp")]
+        public string Otp { get; set; }
+    }
+}
