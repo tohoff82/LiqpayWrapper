7e72761928812ae3ec59b19b665d1f617bb4dd40
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-16 13:39:50 +0200

    Create HoldResponse.cs

---
+++ LiqpayWrapper/LiqpayWrapper/Data/Responses/Acquiring/HoldResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace LiqpayWrapper.Data.Responses.Acquiring
+{
+    public class HoldResponse : AcquirerResponse
+    {
+        [JsonProperty("redirect_to")]
+        public string RedirectTo { get; set; }
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("card_token")]
+        public string CardToken { get; set; }
+    }
+}
